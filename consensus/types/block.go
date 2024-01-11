0557b2c748c062be3a30f299df5aab1a1d079a72
Alejo Acosta <alejoacos@gmail.com>
2024-01-11 11:48:36 -0300

    add functions to proto package to handle generic protobuf conversions

--- consensus/types/block.go
+++ consensus/types/block.go
@@ -1,8 +1,35 @@
 package types
 
+import (
+	"github.com/dominant-strategies/go-quai/p2p/pb"
+)
+
 type Block struct {
 	Hash Hash
 }
+
+// Implementation of the pb.ProtoConvertable for Block
+
+// converts a custom go Block type (types.Block) to a protocol buffer Block type (pb.Block)
+func (b *Block) ToProto() *pb.Block {
+	return &pb.Block{
+		Hash: b.Hash.ToProto(),
+		// TODO: map other fields
+    }
+}
+
+// Implementation of the pb.ConvertableFromProto for Block
+
+// converts a protocol buffer Block type (pb.Block) to a custom go Block type (types.Block)
+func (b *Block) FromProto(pbBlock *pb.Block) {
+	b.Hash.FromProto(pbBlock.Hash)
+}
+
+// returns a new instance of the protocol buffer Block type (pb.Block)
+func (b *Block) NewProtoInstance() *pb.Block {
+	return &pb.Block{}
+}
+
 
 type Slice struct {
 	SliceID SliceID
--- consensus/types/types.go
+++ consensus/types/types.go
@@ -4,11 +4,33 @@ import (
 	"encoding/hex"
 	"strconv"
 
+	"github.com/dominant-strategies/go-quai/p2p/pb"
 	"github.com/pkg/errors"
 )
 
+//*Definitions and implementations for a Hash type*//
+
 // Hash represents a 256 bit hash
 type Hash [32]byte
+
+// Converts a custom go Hash type (types.Hash) to a protocol buffer Hash type (pb.Hash)
+func (h Hash) ToProto() *pb.Hash {
+
+	return &pb.Hash{
+		Hash: h[:],
+	}
+}
+
+// converts a protocol buffer Hash type (pb.Hash) to a custom go Hash type (types.Hash)
+func (h *Hash) FromProto(pbHash *pb.Hash) {
+
+	copy(h[:], pbHash.Hash)
+}
+
+// returns a new instance of the protocol buffer Hash type (pb.Hash)
+func (h Hash) NewProtoInstance() *pb.Hash {
+	return &pb.Hash{}
+}
 
 func NewHashFromString(s string) (Hash, error) {
 	if len(s) != 64 { // 2 hex chars per byte
@@ -23,9 +45,27 @@ func NewHashFromString(s string) (Hash, error) {
 	return hash, nil
 }
 
+//*Definitions and implementations for a Context type*//
+
 type Context struct {
 	Location string
 	Level    uint32
+}
+
+func (c Context) ToProto() *pb.Context {
+	return &pb.Context{
+		Location: c.Location,
+		Level:    c.Level,
+	}
+}
+
+func (c *Context) FromProto(pbContext *pb.Context) {
+	c.Location = pbContext.Location
+	c.Level = pbContext.Level
+}
+
+func (c Context) NewProtoInstance() *pb.Context {
+	return &pb.Context{}
 }
 
 var (
@@ -34,12 +74,34 @@ var (
 	ZONE_CTX   = Context{"zone", 2}
 )
 
+//*Definitions and implementations for a SliceID type*//
+
 type SliceID struct {
 	Context Context
 	Region  uint32
 	Zone    uint32
 }
 
+func (s SliceID) ToProto() *pb.SliceID {
+	return &pb.SliceID{
+		Context: s.Context.ToProto(),
+		Region:  s.Region,
+		Zone:    s.Zone,
+	}
+}
+
 func (sliceID SliceID) String() string {
 	return strconv.Itoa(int(sliceID.Region)) + "." + strconv.Itoa(int(sliceID.Zone))
+}
+
+// converts a protocol buffer SliceID type (pb.SliceID) to a custom go SliceID type (types.SliceID)
+func (sliceID *SliceID) FromProto(pbSliceID *pb.SliceID) {
+	sliceID.Context.FromProto(pbSliceID.Context)
+	sliceID.Region = pbSliceID.Region
+	sliceID.Zone = pbSliceID.Zone
+}
+
+// returns a new instance of the protocol buffer SliceID type (pb.SliceID)
+func (sliceID SliceID) NewProtoInstance() *pb.SliceID {
+	return &pb.SliceID{}
 }
