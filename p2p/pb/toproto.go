944e2d8327e23c30082c5b4275d2d1f55e14e40c
Alejo Acosta <alejoacos@gmail.com>
2024-01-22 13:27:30 -0300

    implement conversion from types.Header to pb.Header

--- p2p/pb/toproto.go
+++ p2p/pb/toproto.go
@@ -12,7 +12,57 @@ func convertBlockToProto(block *types.Block) *Block {
 
 // Converts a custom Header type to a protobuf Header type
 func convertHeaderToProto(header *types.Header) *Header {
-	panic("TODO: implement")
+	if header == nil {
+		return nil
+	}
+
+	protoHeader := &Header{
+		UncleHash:     header.UncleHash().Bytes(),
+		Coinbase:      header.Coinbase().Bytes(),
+		Root:          header.Root().Bytes(),
+		TxHash:        header.TxHash().Bytes(),
+		EtxHash:       header.EtxHash().Bytes(),
+		EtxRollupHash: header.EtxRollupHash().Bytes(),
+		ReceiptHash:   header.ReceiptHash().Bytes(),
+		Difficulty:    header.Difficulty().Bytes(),
+		GasLimit:      header.GasLimit(),
+		GasUsed:       header.GasUsed(),
+		BaseFee:       header.BaseFee().Bytes(),
+		Location:      header.Location(),
+		Time:          header.Time(),
+		Extra:         header.Extra(),
+		MixHash:       header.MixHash().Bytes(),
+		Nonce:         header.Nonce().Bytes(),
+	}
+	// Convert []*big.Int fields with HierarchyDepth
+	for i := 0; i < common.HierarchyDepth; i++ {
+		if header.ParentEntropy(i) != nil {
+			protoHeader.ParentEntropy = append(protoHeader.ParentEntropy, header.ParentEntropy(i).Bytes())
+		}
+		if header.ParentDeltaS(i) != nil {
+			protoHeader.ParentDeltaS = append(protoHeader.ParentDeltaS, header.ParentDeltaS(i).Bytes())
+		}
+
+		if header.Number(i) != nil {
+			protoHeader.Number = append(protoHeader.Number, header.Number(i).Bytes())
+		}
+	}
+
+	// Convert array of parent hashes
+	parentHash := make([][]byte, len(header.ParentHashArray()))
+	for i, hash := range header.ParentHashArray() {
+		parentHash[i] = hash.Bytes()
+	}
+	protoHeader.ParentHash = parentHash
+
+	// Convert array of manifest hashes
+	manifestHash := make([][]byte, len(header.ManifestHashArray()))
+	for i, hash := range header.ManifestHashArray() {
+		manifestHash[i] = hash.Bytes()
+	}
+	protoHeader.ManifestHash = manifestHash
+
+	return protoHeader
 }
 
 // Converts a custom Transaction type to a protobuf Transaction type
