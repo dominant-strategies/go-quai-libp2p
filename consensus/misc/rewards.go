b067f8d6e2c73832214a15688d5062f36eb6a74a
drk <joe@dominantstrategies.io>
2024-02-23 10:34:11 -0600

    Calculate Reward stub function for Qi and Quai

--- consensus/misc/rewards.go
+++ consensus/misc/rewards.go
@@ -3,12 +3,25 @@ package misc
 import (
 	"math/big"
 
+	"github.com/dominant-strategies/go-quai/common"
 	"github.com/dominant-strategies/go-quai/core/types"
 )
 
 // CalculateReward calculates the coinbase rewards depending on the type of the block
 func CalculateReward(header *types.Header) *big.Int {
-	//// This Reward Schedule is only for Iron Age Testnet and has nothing to do
-	//// with the Mainnet Schedule
-	return new(big.Int).Mul(header.Difficulty(), big.NewInt(10e8))
+	if header.Coinbase().IsInQiLedgerScope() {
+		return calculateQiReward(header)
+	} else {
+		return calculateQuaiReward(header)
+	}
+}
+
+// CalculateQuaiReward calculates the quai that can be recieved for mining a block
+func calculateQuaiReward(header *types.Header) *big.Int {
+	return common.Big0
+}
+
+// CalculateQiReward caculates the qi that can be received for mining a block
+func calculateQiReward(header *types.Header) *big.Int {
+	return common.Big0
 }
