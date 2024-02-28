4942724fa0e7a943dad4c2339b801d298f618019
drk <joe@dominantstrategies.io>
2024-02-28 17:49:51 -0600

    WIP: Added types to wo and first setter and getter

--- core/types/wo.go
+++ core/types/wo.go
@@ -6,11 +6,33 @@ import (
 	"github.com/dominant-strategies/go-quai/common"
 )
 
-type workObject struct {
-	proposedHeader PendingHeader
-	parentHash     common.Hash
-	parentNumber   big.Int
-	difficulty     big.Int
-	txHash         common.Hash
-	nonce          BlockNonce
+type WorkObject struct {
+	woHeader WorkObjectHeader
+	woBlock  WorkObjectBlock
+	tx       Transaction
+}
+
+type WorkObjectHeader struct {
+	proposedHeaderHash common.Hash
+	parentHash         common.Hash
+	parentNumber       *big.Int
+	difficulty         *big.Int
+	txHash             common.Hash
+	nonce              BlockNonce
+}
+
+type WorkObjectBlock struct {
+	header          *Header
+	transactions    Transactions
+	extTransactions Transactions
+	uncles          []*Header
+	manifest        BlockManifest
+}
+
+func (wo *WorkObject) Header() *Header {
+	return wo.woBlock.header
+}
+
+func (wo *WorkObject) SetProposedHeader(header *Header) {
+	wo.proposedHeader = CopyHeader(header)
 }
